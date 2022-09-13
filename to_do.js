7c2d51665ee0f662c02f14ee1b708033d94de6e6
Tammy Freire <tammy.freire@gmail.com>
2022-09-13 15:12:18 +0200

    To-do project

---
+++ to_do.js
@@ -0,0 +1,59 @@
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+function askquestion(text){
+  return new Promise(function(resolve, rej){
+    console.log(text);
+    readline.on('line', (task) => {
+      resolve(task);
+    });
+  });
+}
+
+async function yesOrNo(text){
+  var answer = await askquestion(text);
+  if(answer == "yes") {
+    return true;
+  } else if(answer == "no"){
+    return false
+  } else {
+    console.log("Answer not valid");
+    return await yesOrNo(text);
+  }
+}
+
+async function main() {
+  let todo = []
+
+  var answer = await yesOrNo("Create todo?")
+  console.log("Answer:", answer)
+  
+  var item = await askquestion("What is the task?")
+  todo.push(item);
+  console.log("Task:", item);
+
+  while (answer = await yesOrNo("Add another task?")) {
+    console.log("Answer:", answer)
+
+    var item = await askquestion("What is the task?")
+    todo.push(item);
+    console.log("Task:", item);
+  }
+
+  console.log('This is your to-do list:');
+
+  todo.forEach((item)=> {
+    console.log(item)
+  })
+  
+  process.exit();
+}
+
+main()
+
+
+
